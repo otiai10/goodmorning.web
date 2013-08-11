43f8eb50da3f56833a24babd76ea0d4e0d740985
otiai10 <otiai10@gmail.com>
2013-08-11 20:02:58 +0900

    web embed

--- js/main.js
+++ js/main.js
@@ -1,19 +1,24 @@
-
-var base_str = '<li><div class="entry"><iframe src="{url}" width="300" height="80"></iframe></div></li>';
+var base_str = '<li class="entry"><ul><li><p><span>{artistName} : <a href="{trackViewUrl}">{trackName}</a></span></p></li><li><iframe src="{previewUrl}" width="400" height="80"></iframe></li></ul></li>';
 $(function(){
   $.ajax({
-    url      : 'https://itunes.apple.com/search?term=jack+johnson&limit=5',
+    url      : 'https://itunes.apple.com/search?term=jack+johnson&limit=5&entity=musicVideo&country=jp',
     dataType : 'JSONP',
     success  : function(data) {
       console.log("Success:",data);
       var entries = data.results;
       var len = entries.length;
       for(var i=0;i<len;i++){
-        $('ol#results').append(base_str.replace('{url}', entries[i].previewUrl));
+        $('ol#results').append(
+            base_str
+                .replace('{artistName}', entries[i].artistName)
+                .replace('{trackName}', entries[i].trackName)
+                .replace('{trackViewUrl}', entries[i].trackViewUrl)
+                .replace('{previewUrl}', entries[i].previewUrl)
+        );
       }
     },
     error    : function(err) {
-      console.log("Error:",err);
-    },
+      console.log("Error:", err);
+    }
   });
 });
